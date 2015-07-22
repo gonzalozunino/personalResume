80a4e9b89ec07677da6e99701b4646fd516da3f4
Gonzalo Alejandro Zunino <gonzalo.zunino@glb-d3009.globant.com>
2015-07-22 12:33:22 -0300

    Add contact controller postmark

--- client/contact/contact.controller.ng.js
+++ client/contact/contact.controller.ng.js
@@ -1,6 +1,42 @@
 'use strict';
 
 angular.module('personalResumeApp')
-.controller('ContactCtrl', function($scope) {
-  $scope.viewName = 'Contact';
+.controller('ContactCtrl', function($scope, $http) {
+	$scope.viewName = 'Contact';
+	$scope.success = false;
+	$scope.error = false;
+
+	$scope.send = function () {
+
+		var htmlBody = '<html><body><div>Name: ' + $scope.msg.name + '</div>' +					   
+					   '<div>Message: ' + $scope.msg.body + '</div>' +
+					   '<div>Date: ' + (new Date()).toString() + '</div></body></html>';
+
+		var textBody = 'Name: ' + $scope.msg.name + '\n' +		
+				'Message: ' + $scope.msg.body + '\n' +
+				'Date: ' + (new Date()).toString() + '\n';
+
+		$http({
+			url: 'https://api.postmarkapp.com/email',
+			method: 'POST',
+			data: {
+				'From': 'contact@gonzalozunino.com.ar',
+				'To': 'gonzalo.zunino@gmail.com',
+				'ReplyTo' : $scope.msg.email,
+				'Subject': $scope.msg.subject,
+				'HtmlBody': htmlBody,
+				'TextBody': textBody
+			},
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+				'X-Postmark-Server-Token': '39531bdb-c835-4395-8e96-703ec0beaa7a'
+			}
+	    }).success(function (data) {
+	    	$scope.success = true;
+	    	$scope.user = {};
+	    }).error(function (data) {
+	    	$scope.error = true;
+	    });
+	};
 });
