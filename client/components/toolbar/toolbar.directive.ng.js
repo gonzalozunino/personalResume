6abf316af8d831ee82350a7aeea6d78f939f9c1c
Gonzalo Zunino <gonzalo.zunino@gmail.com>
2015-07-27 21:54:20 -0300

    Contact and animations changes

--- client/components/toolbar/toolbar.directive.ng.js
+++ client/components/toolbar/toolbar.directive.ng.js
@@ -1,10 +1,10 @@
 'use strict'
 
 angular.module('personalResumeApp')
-.directive('toolbar', function(scope, elm, attrs) {
-  return {
-    restrict: 'AE',
-    templateUrl: 'client/components/toolbar/toolbar.view.ng.html',
-    replace: true
-  };
+.directive('toolbar', function() {	
+	return {
+		restrict: 'AE',
+		templateUrl: 'client/components/toolbar/toolbar.view.ng.html',
+		replace: true
+	};  
 });
